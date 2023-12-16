ae6c33929675c8e232cd93d5f77ff46bbdb57913
IMG\enzo.turano <enzo.turano@img.com.br>
2023-12-16 05:19:43 -0300

    feat: making studentView a dumb component

---
+++ front-end/src/components/studentView/studentView.tsx
@@ -0,0 +1,91 @@
+import { Providers } from "@/app/providers";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  Stepper,
+  Step,
+  StepIndicator,
+  StepStatus,
+  StepIcon,
+  StepNumber,
+  StepTitle,
+  StepDescription,
+  StepSeparator,
+  Box,
+} from "@chakra-ui/react";
+import styles from "./studentView.module.css";
+import { FC } from "react";
+
+interface StudentViewProps {
+  intitutions: institution[];
+}
+
+export interface institution {
+  name: string;
+  Series: Array<serie>;
+}
+
+export interface serie {
+  title: string;
+  status: string;
+}
+
+const StudentView: FC<StudentViewProps> = ({ intitutions }) => {
+  return (
+    <>
+      <Providers>
+        <div>
+          <p className={styles.schoolType}>Instituições</p>
+        </div>
+        {intitutions.map((data) => {
+          return (
+            <>
+              <Accordion allowMultiple allowToggle>
+                <AccordionItem>
+                  <AccordionButton>
+                    <Box as="span" flex="1" textAlign="left">
+                      {data.name}
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+
+                  <AccordionPanel>
+                    <Stepper
+                      index={4}
+                      orientation="vertical"
+                      height="400px"
+                      gap={0}
+                      marginLeft={6}
+                    >
+                      {data.Series.map((step, index) => (
+                        <Step key={index}>
+                          <StepIndicator>
+                            <StepStatus
+                              complete={<StepIcon />}
+                              incomplete={<StepNumber />}
+                              active={<StepNumber />}
+                            />
+                          </StepIndicator>
+                          <Box flexShrink="0">
+                            <StepTitle>{step.title}</StepTitle>
+                            <StepDescription>{step.status}</StepDescription>
+                          </Box>
+                          <StepSeparator />
+                        </Step>
+                      ))}
+                    </Stepper>
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </>
+          );
+        })}
+      </Providers>
+    </>
+  );
+};
+
+export default StudentView;
