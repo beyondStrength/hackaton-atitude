7f77b6ca2a16b8332a57e2ff5b590e81e49976e7
igor <igorrturano@gmail.com>
2023-12-16 06:16:03 -0300

    Adiciona swagger

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentController } from './student/controllers/student.controller';
+import { StudentService } from './student/service/student.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, StudentController],
+  providers: [AppService, StudentService],
 })
 export class AppModule {}
--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('scholar.id')
+    .setDescription('Sua API para convergência de dados educacionais')
+    .setVersion('1.0')
+    .addTag('student')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
